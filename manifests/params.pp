bd8abea1a8b87ad1c900a53e194f52122d555d08
Elzair <elzairthesorcerer@gmail.com>
2014-02-05 11:54:53 -0500

    Removed trailing commas in manifests/params.pp

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,7 +1,7 @@
 class dtrace4linux::params {
-  $dev_dir = "/home/vagrant/devel",
-  $user = "vagrant",
-  $group = "vagrant",
-  $distro = "ubuntu",
-  $module_file = "/etc/modules",
+  $dev_dir = "/home/vagrant/devel"
+  $user = "vagrant"
+  $group = "vagrant"
+  $distro = "ubuntu"
+  $module_file = "/etc/modules"
 }
